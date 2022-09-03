34d185321c20e2e80748fc90042b6af7cce8acaf
Roni <107792221+conglome@users.noreply.github.com>
2022-09-03 19:26:49 +0300

    Update static-av.py

--- static-av.py
+++ static-av.py
@@ -1,4 +1,5 @@
 import hashlib
+import os
 
 def file_as_bytes(file):
     with file:
@@ -6,16 +7,23 @@ def file_as_bytes(file):
 
 print("Welcome to static-av 1.0")
 
-virus_file_name = input("File Name : ")
+virus_file_name = input("File Name (with extension): ")
 hashlist_file_name = input("Hash list file name : ")
 virus_signature = hashlib.md5(file_as_bytes(open(virus_file_name, 'rb'))).hexdigest()
 
 with open(hashlist_file_name , 'r') as scan:
+
     for sign in scan:
+        
         print("Comparing - > " , virus_signature ,"with", sign)
         if (virus_signature == sign):
-            print("File Malicious. Quitting...")
-            quit()
+
+            malicious = input("File is malicious, do you want to delete this file ? (y/n) ")
+            if (malicious == 'y'):
+                os.system("shred -f -z -u -n 20 ", virus_file_name)
+            else:
+                print("Quitting...")
+                quit()
         
     print("File is not Malicious. Exitting...")
     quit()
